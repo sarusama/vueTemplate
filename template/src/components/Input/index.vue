99403fe5b4b697ccc0f862bf0bb273b3e4e0be10
sarusama <ld839183343@163.com>
2020-04-21 21:23:24 +0800

    [update] vue teemplate

--- template/src/components/Input/index.vue
+++ template/src/components/Input/index.vue
@@ -4,6 +4,8 @@
 		<input 
 			v-bind:type="type" 
 			v-bind:name="name"
+			v-on:input="input"
+			v-on:change="change"
 			/>
 	</form>
 </template>
@@ -14,15 +16,38 @@ export default {
 	data: function () {
 		return {};
 	},
-	props: {},
+	props: {
+		name: {
+			type: String,
+			default: ""
+		},
+		type: {
+			type: String,
+			default: "text"
+		},
+		maxlength: {
+			type: Number,
+			default: -1
+		},
+		value: {
+			type: String,
+			default: ""
+		}
+	},
 	methods: {
 		action: function () {
 			return false;
+		},
+		input: function () {
+			this.$emit("input");
+		},
+		change: function () {
+			this.$emit("change");
 		}
 	}
 }
 </script>
 
-<style scoped="">
+<style scoped>
 @import "./index.css";
 </style>
